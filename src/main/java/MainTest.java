b5a350ce58ee28b89fa6292a26f075cc32123a44
Ac Quy Khong Ten <nhuconghaui@gmail.com>
2019-09-16 15:09:11 +0700

    lop 2d va 3d

---
+++ src/main/java/MainTest.java
@@ -0,0 +1,9 @@
+public class MainTest {
+    public static void main(String[] args) {
+        MainTest mainTest = new MainTest();
+        mainTest= new Point2D(3.5f, 2.6f);
+        System.out.println(mainTest);
+        mainTest= new Point3D(2f,4f,2.7f);
+        System.out.println(mainTest);
+    }
+}
